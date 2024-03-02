2a487b411aafe45a9d59340a44d64188cc0fa830
Dheeraj kumar mahto <126415291+Dheerajmahto62@users.noreply.github.com>
2024-03-02 21:24:20 +0530

    first commit

--- Questions/Palindrom_No.c
+++ Questions/Palindrom_No.c
@@ -8,7 +8,7 @@ int main()
     int n = sizeof(arr) / sizeof(arr[0]);
 
     int temp = 0;
-    for (int i = 0; i < n / 2; i++)
+    for (int i = 0; i <  n / 2; i++)
     {
         if (arr[i] == arr[n - i - 1])
         {
